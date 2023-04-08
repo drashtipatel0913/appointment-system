16dfeeb2d02b3eb85293ebc4cbb177fb018fa137
drshty <drashtipatel0913@gmail.com>
2023-04-08 11:24:28 -0400

    created form in `appointment.html`, added code in `main.js` that retrive data entered by user and get focus back to on name field, added validation for all the values

--- main.js
+++ main.js
@@ -17,20 +17,20 @@ function getCurrentDay() {
 // returns current time
 function getCurrentTime() {
 
-  // Updates meridiem based on the hour
-  let meridiem = "";
-  if (hours >= 12) {
-     hours = hours - 12;
-     meridiem = "PM";
-  } else {
-     meridiem = "AM";
-  }
-  
+   // Updates meridiem based on the hour
+   let meridiem = "";
+   if (hours >= 12) {
+      hours = hours - 12;
+      meridiem = "PM";
+   } else {
+      meridiem = "AM";
+   }
+
    return `${padSingleDigit(hours)} : ${padSingleDigit(minutes)}  ${meridiem}`;
 }
 
 // adds zero before single digits
-function padSingleDigit (num) {
+function padSingleDigit(num) {
    if (num < 10) {
       return "0" + num;
    } else {
@@ -45,3 +45,30 @@ currentDay.innerHTML = `${getCurrentDay()}`;
 // displays the current time
 const currentTime = $("currentTime");
 currentTime.innerHTML = getCurrentTime();
+
+// saves all the form input data to variables 
+const _name = $('name');
+const purpose = $('purpose');
+const appointmentForm = $('appointmentForm');
+const dateInput = $('date');
+const timeInput = $('time');
+
+// focuses on name as user comes to the page
+_name.focus();
+
+// returns true if all the form field has value otherwise returns fals
+function isFormValid() {
+   let isFormValid = true;
+
+   if (_name.value == '') {
+      isFormValid = false;
+   } else if (purpose.value == '') {
+      isFormValid = false;
+   } else if (dateInput.value == '') {
+      isFormValid = false;
+   } else if (timeInput.value == '') {
+      isFormValid = false;
+   }
+
+   return isFormValid;
+}
